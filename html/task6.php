fcd4f4fc3e3d17af6ec158735cfb7902650c5122
chandukumarM <chandu3319@gmail.com>
2017-12-17 14:01:12 +0530

    Signed-off-by: chandukumarM <chandu3319@gmail.com>

---
+++ html/task6.php
@@ -0,0 +1,152 @@
+<DOCTYPE html>
+<html>
+          <head>
+           <title>task6</title>
+           <link href="../css/task6.css" rel="stylesheet" type="text/css"/>
+            <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+			<meta charset="UTF-8"/>
+			<meta name="keywords"content="web desigining,web development,website desigining"/>
+			<meta name="discription" content="we are......."/>
+			<meta name="author" content="zch"/>
+			<meta name="viewport"content="width=device-width,initial-scale=1.0"/>
+          </head>
+            <body>
+			     <div class="header">
+				          <div class="topbar">
+					           <i class="fa fa-mobile"><p>1800 1800 1800 toolfree</p></i>
+					           <i class="fa fa-envelope"><p>zch@gmail.com</p></i>
+					           <i class="fa fa-internet-explorer"><p>www.zch.com</p></i>
+					       </div>
+					
+					
+					
+					<div class="navbar">
+					     <div class="vv"><i class="fa fa-tachometer"></i>dashboard
+						 </div>
+					     <div class="brand"><i class="fa fa-graduation-cap"></i>
+						 </div>
+						 <div>
+						
+						
+						
+						<div class="navmenu">
+						<ul>
+						<li><a href="http://zerocosthiring.com/">Home</a></li>
+						<li><a href="http://zerocosthiring.com/">About</a></li>
+						<li>Contact</li>
+						<li>help</li>
+						</ul>
+						</div>
+					
+					</div>
+				
+				
+				
+				
+				</div>
+			
+			
+			
+			
+			
+			<div class="section">
+			
+			
+			
+			<div class="sidenav">
+			      <div class="nav1">
+			           <h4>compose</h4>
+			             <hr>
+			              <h4>important</h4>
+			              <hr>
+			             <h4>contacts</h4>
+			              <hr>
+			            <h4>tags</h4>
+			            <hr>
+			      </div>
+			          <div class="nav2">
+			              <h4>personal</h4>
+			               <hr>
+					        <h4>links</h4>
+			                <hr>
+					        <h4>subtitles</h4>
+			                <hr>
+					       <h4>maps</h4>
+			               <hr>
+						   <h4>links</h4>
+			                <hr>
+			         </div>
+			</div>
+			
+			
+			
+			
+		<div class="article">
+		    <div class="article1">
+		                   <div class="a">Gradiant is a technology driven water company specializing 
+			                in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio of proprietary water treatment products and award-winning service. 
+			              Our global installation base improves customer ... in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio  in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio 
+		                 </div>
+		
+		             <div class="b">
+					 <img src="ram17.jpg">
+					 
+					 <h3>Honda Goldwing</h3>
+					 
+		             </div>
+		                     <div class="c">
+							 Gradiant is a technology driven water company specializing 
+			                in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio of proprietary water treatment products and award-winning service. 
+			              Our global installation base improves customer ... in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio  in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio  in industrial water treatment and recycling solutions. Founded at Massachusetts Institute of Technology MIT
+			              , Gradiant has built a strong portfolio 
+							 </div>
+			</div>
+		
+		
+		
+		<div class="article2">
+		<div class="d"></div>
+		<div class="e"></div>
+		<div class="f"></div>
+		
+		</div>
+		<div class="clear">
+		
+		</div>
+		
+		
+		
+		<div class="footer1">
+		</div>
+		<div class="footer2">
+		<h4 <i class="fa fa-copyright"></i> copyright by www.zerocost.com</h4>
+		<h3  <i class="fa fa-facebook-square"><i class="fa fa-twitter-square"><i class="fa fa-envelope"></i>
+</i>
+</i>contact us</h3>
+		
+
+		</div>
+		</div>
+		
+		
+		</div>
+		
+			
+			
+			</body>
+
+
+
+
+
+
+
+
+</html>
